a480a9f8cf84cfe38bed9263b8415168ac34c55c
Evgenii <pitohuip@yahoo.com>
2022-07-17 05:59:50 +0000

    add new pages

--- myapp.rb
+++ myapp.rb
@@ -8,6 +8,23 @@ get '/navigation' do
   haml :'navigation/cpp'
 end
 
+get '/qt_navigation' do
+  haml :'navigation/qt'
+end
+
+get '/contact_me' do
+  haml :contact_me
+end
+
+# PAGES
 get '/navigation/vars' do
   markdown :'pages/variables', :layout_engine => :haml, :layout => :layout_md
+end
+
+get '/navigation/data_types' do
+  markdown :'pages/data_types', :layout_engine => :haml, :layout => :layout_md
+end
+
+get '/navigation/type_conversions' do
+  markdown :'pages/type_conversions', :layout_engine => :haml, :layout => :layout_md
 end
